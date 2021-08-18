c0568b09fbae573dae025f5ddf1a8e454b7fa550
Fatih20 <fnri39@protonmail.com>
2021-08-18 21:24:11 +0700

    Functionally complete

---
+++ docs/Javascript/end_game.js
@@ -0,0 +1,9 @@
+end_game_conversion = {
+    "0 Start A" : "It's your day off and you have nothing to do at home.",
+	"1 Bored_At_Home A" : "You decided to go to the restaurant to shave off some boredom, get some food, and maybe enjoy new atmosphere. It's an italian restaurant with good lighting, good music, and a good mood. The waiter come up to you and asked what you want to order.",
+	"1 Bored_At_Home B": "You decided to go to the cinema to watch the latest and greatest movie that people have been talking about. And also because you have nothing to do at home. In the cinema, there were barely anyone here. It is a weekday in the afternoon, so it's what you should expect. Once you go to the ticket booth, there are only 2 movies playing because not many people go to the cinema in weekday afternoon. The options are Jaws and Star Wars",
+	"2 Restaurant A" : "You ordered a pasta. It arrived in about 15 minutes. The delicious pasta and the great mood of the restaurant make you enjoy yourself very much.",
+	"2 Restaurant B" : "You ordered a spaghetti. It arrived in about 15 minutes.",
+	"2 Cinema A" : "You decided to watch Jaws. It's a pretty thrilling and scary movie. You wound up having thalassophobia.",
+	"2 Cinema B" : "You decided to watch Star Wars. The space adventure Luke and the gang goes through keeps you at the edge of your seat. You really enjoy the movie and can't wait for the sequel.",
+}; 
--- docs/Javascript/main.js
+++ docs/Javascript/main.js
@@ -7,7 +7,7 @@ start_button.addEventListener('click', function(){
     console.log("click");
     change_flex_direction_play_area("row");
     const title = document.querySelector(".title");
-    title.innerHTML = "Enjoy your adventure"
+    title.innerHTML = "Enjoy your adventure";
     director("A");
 });
 
@@ -25,14 +25,19 @@ function change_flex_direction_play_area (direction) {
 function director (signal) {
     const current_event = event_name_conversion[path_taken[path_taken.length-1][1]];
     const nth_current_event = path_taken[path_taken.length-1][0];
-    for (answer_for_next_event of current_event.Answers_For_Next_Event_List){
-        if (signal === answer_for_next_event.trigger) {
-            const next_event = event_name_conversion[answer_for_next_event.next_event_name];
-            path_taken.push([nth_current_event+1, answer_for_next_event.next_event_name]);
-            update_play_area(next_event);
+    path_taken[path_taken.length-1].push(signal);
+    if (current_event.Answers_For_Next_Event_List.length === 0) {
+        generate_end_game();
+    } else {
+        for (answer_for_next_event of current_event.Answers_For_Next_Event_List){
+            if (signal === answer_for_next_event.trigger) {
+                const next_event = event_name_conversion[answer_for_next_event.next_event_name];
+                path_taken.push([(parseInt(nth_current_event)+1).toString(), answer_for_next_event.next_event_name]);
+                update_play_area(next_event);
+                break;
+            }
         }
     }
-    // generate_end_game();
 
 };
 
@@ -69,5 +74,30 @@ function update_play_area (next_event) {
     }
     play_area.appendChild(event);
     play_area.appendChild(choice_container);
+};
+
+function generate_end_game (){
+    let path_summation = "";
+    console.log(path_taken);
+    for (path of path_taken) {
+        path_summation += end_game_conversion[path.join(" ")];
+        if (path_taken.indexOf(path) !== path_taken.length - 1){
+            path_summation += " ";
+        }
+    }
+    const title = document.querySelector(".title");
+    title.innerHTML = "The path you've taken";
+
+    const play_area = document.querySelector(".play-area");
+    play_area.innerHTML = "";
+    play_area.classList.remove("play-area-game");
+    play_area.classList.add("play-area-end");
+
+    const end_game = document.createElement('p');
+    end_game.className = "end-game";
+    end_game.innerHTML = path_summation;
+
+    play_area.appendChild(end_game);
+
 };
 
