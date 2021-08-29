8888d34c5a2d1e6278cd57e107da848f6eceac9f
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2021-08-29 10:32:56 +0700

    Resturcture Events class, generate_end_story, paragraph_determiner and cut functions that are obsolete due to restructuring

--- docs/Javascript/imported_modules/event.js
+++ docs/Javascript/imported_modules/event.js
@@ -5,9 +5,17 @@ export class Event {
 		this.Question = event_attribute.Question;
 		this.Possible_Answer_List = event_attribute.Possible_Answer_List;
 		this.Answers_For_Next_Event_List = event_attribute.Answers_For_Next_Event_List;
-		this.Default_Ending_Bit = event_attribute.Default_Ending_Bit;
-		this.Default_Paragraph = event_attribute.Default_Paragraph;
-		this.Conditions = event_attribute.Conditions;
+
+		for (let choice in event_attribute.Ending) {
+			for (let ending_bit of event_attribute.Ending[choice]){
+				if (ending_bit.paragraph === undefined) {
+					ending_bit.paragraph = "none";
+				}
+			}
+		}
+		this.Ending = event_attribute.Ending;
+		console.log(this.Ending)
+
     }
 };
 
@@ -18,8 +26,14 @@ export const Start = new Event({
 	Possible_Answer_List : [],
 	// Next_event_name should be the first event in the game
 	Answers_For_Next_Event_List : [{trigger : "A", next_event_name: "Bored_At_Home"}],
-	Default_Ending_Bit: {
-		"A" : "It's your day off and you have nothing to do at home."},
+	Ending : {
+		"A" : [
+			{
+			type : "default_ending",
+			story_bit : "It's your day off and you have nothing to do at home." 
+			},
+		]
+	}
 });
 
 export const Bored_At_Home = new Event({
@@ -28,12 +42,20 @@ export const Bored_At_Home = new Event({
 	Question : "What do you do?",
 	Possible_Answer_List : [{id : "A", answer: "Go to a restaurant" }, {id : "B", answer: "Go to the cinema"}],
 	Answers_For_Next_Event_List : [{trigger: "A", next_event_name : "Restaurant"}, {trigger : "B", next_event_name : "Cinema"}],
-	Default_Ending_Bit: {
-		"A" : "You decided to go to the restaurant to shave off some boredom, get some food, and maybe enjoy new atmosphere. It's an italian restaurant with good lighting, good music, and a good mood. The waiter come up to you and asked what you want to order.",
-		"B" : "You decided to go to the cinema to watch the latest and greatest movie that people have been talking about. And also because you have nothing to do at home. In the cinema, there were barely anyone here. It is a weekday in the afternoon, so it's what you should expect. Once you go to the ticket booth, there are only 2 movies playing because not many people go to the cinema in weekday afternoon. The options are Jaws and Star Wars"},
-	Default_Paragraph : {
-		"A" : "last sentence",
-		"B" : "new paragraph"}
+	Ending : {
+		"A": [
+			{
+			type: "default_ending",
+			story_bit : "You decided to go to the restaurant to shave off some boredom, get some food, and maybe enjoy new atmosphere. It's an italian restaurant with good lighting, good music, and a good mood. The waiter come up to you and asked what you want to order.",
+			}
+		],
+		"B" : [
+			{
+			type: "default_ending",
+			story_bit : "You decided to go to the cinema to watch the latest and greatest movie that people have been talking about. And also because you have nothing to do at home. In the cinema, there were barely anyone here. It is a weekday in the afternoon, so it's what you should expect. Once you go to the ticket booth, there are only 2 movies playing because not many people go to the cinema in weekday afternoon. The options are Jaws and Star Wars."
+			}
+		]
+	}
 });
 
 export const Restaurant = new Event({
@@ -42,16 +64,26 @@ export const Restaurant = new Event({
 	Question : "What do you order?",
 	Possible_Answer_List : [{id : "A", answer: "Pasta" }, {id : "B", answer: "Spaghetti"}],
 	Answers_For_Next_Event_List : [{trigger: "A", next_event_name : "End"}, {trigger : "B", next_event_name : "End"}],
-	Default_Ending_Bit: {
-		"A" : "You ordered a pasta. It arrived in about 15 minutes. The delicious pasta and the great mood of the restaurant make you enjoy yourself very much.",
-		"B" : "You ordered a spaghetti. It arrived in about 15 minutes."},
-	Default_Paragraph : {"A" : "last sentence", "B" : "new paragraph"},
-	Conditions : {
+	Ending : {
+		"A" : [
+			{
+			type: "default_ending", 
+			story_bit : "You decided to go to the restaurant to shave off some boredom, get some food, and maybe enjoy new atmosphere. It's an italian restaurant with good lighting, good music, and a good mood. The waiter come up to you and asked what you want to order.",
+			paragraph : "last sentence"}
+		],
 		"B" : [
-			{type: "nth_event_checker", specification : {
-				event_before : "Bored_At_Home",
-			}, end_game_story_bit : "Because you were bored at home, you decided to order a spaghetti.", paragraph : "last sentence"}
-	]}
+			{
+			type: "nth_event_checker", 
+			specification : {
+				event_before : "Bored_At_Home"}, 
+			end_game_story_bit : "Because you were bored at home, you decided to order a spaghetti.",
+			paragraph : "last sentence"},
+
+			{type: "default_ending", 
+			story_bit : "You decided to go to the cinema to watch the latest and greatest movie that people have been talking about. And also because you have nothing to do at home. In the cinema, there were barely anyone here. It is a weekday in the afternoon, so it's what you should expect. Once you go to the ticket booth, there are only 2 movies playing because not many people go to the cinema in weekday afternoon. The options are Jaws and Star Wars",
+			paragraph : "new paragraph"}
+		],
+	}
 });
 
 export const Cinema = new Event({
@@ -60,12 +92,18 @@ export const Cinema = new Event({
 	Question : "What movie do you want to see?",
 	Possible_Answer_List : [{id : "A", answer: "Jaws" }, {id : "B", answer: "Star Wars"}],
 	Answers_For_Next_Event_List : [{trigger: "A", next_event_name : "End"}, {trigger : "B", next_event_name : "End"}],
-	Default_Ending_Bit: {
-		"A" : "You decided to watch Jaws. It's a pretty thrilling and scary movie. You wound up having thalassophobia.",
-		"B" : "You decided to watch Star Wars. The space adventure Luke and the gang goes through keeps you at the edge of your seat. You really enjoy the movie and can't wait for the sequel."},
-	Default_Paragraph : {
-		"A" : "last sentence",
-		"B" : "new paragraph"}
+	Ending : {
+		"A": [
+			{type: "default_ending",
+			story_bit : "You decided to watch Jaws. It's a pretty thrilling and scary movie. You wound up having thalassophobia.",
+			paragraph : "last sentence"}
+		],
+		"B" : [
+			{type: "default_ending",
+			story_bit : "You decided to watch Star Wars. The space adventure Luke and the gang goes through keeps you at the edge of your seat. You really enjoy the movie and can't wait for the sequel.",
+			paragraph : "new paragraph"}
+		]
+	}
 });
 
 export const event_name_conversion = {
