7c2975afa3dc331a1816ca3ad0ab40edbe334d63
Fatih20 <fnri39@protonmail.com>
2021-09-03 14:25:32 +0700

    Encapsulate functions that contribute to end game creation in the event class

--- docs/Javascript/imported_modules/end_game.js
+++ docs/Javascript/imported_modules/end_game.js
@@ -57,9 +57,9 @@ function generate_end_story (path_taken) {
         const examined_event = event_file.event_name_conversion[path.name_of_event];
         let story_bit_and_paragraph_type;
         if (examined_path_list.length === path_taken.length-1){
-            story_bit_and_paragraph_type = end_story_bit_generator(examined_path_list, path, examined_event, paragraph_type_ledger, true);
+            story_bit_and_paragraph_type = examined_event.end_story_bit_generator(examined_path_list, path, examined_event, paragraph_type_ledger, true);
         } else {
-            story_bit_and_paragraph_type = end_story_bit_generator(examined_path_list, path, examined_event, paragraph_type_ledger);
+            story_bit_and_paragraph_type = examined_event.end_story_bit_generator(examined_path_list, path, examined_event, paragraph_type_ledger);
         }
 
         path_summation_list.push(story_bit_and_paragraph_type.story_bit);
@@ -73,90 +73,5 @@ function generate_ending (path_taken){
     let path = path_taken[path_taken.length-1];
     const examined_path_list = path_taken.slice(0, path_taken.length-1);
     const examined_event = event_file.event_name_conversion[path.name_of_event];
-    return end_story_bit_generator(examined_path_list, path, examined_event, [], true).story_bit;
-};
-
-function paragraph_determiner (end_game_story_bit, paragraph_type, paragraph_type_ledger){
-    let result;
-    let break_before = true;
-    if (paragraph_type_ledger[paragraph_type_ledger.length-1] === "last sentence"){
-        no_break_before = false;
-    }
-
-    if (paragraph_type === "none" || paragraph_type === "new paragraph"){
-        result = end_game_story_bit;
-    } else if (paragraph_type === "last sentence" || paragraph_type === "standalone paragraph"){
-        result = end_game_story_bit+"<br><br>";
-    }
-
-    if (paragraph_type === "standalone paragraph" || paragraph_type === "new paragraph"){
-        if (break_before){
-            result = "<br><br>"+result;
-        }
-    }
-
-    return result;
-};
-
-function end_story_bit_generator (previously_examined_path_list, currently_examined_path, examined_event, paragraph_type_ledger, ignore_paragraph=false){
-    const condition_list = examined_event.Ending[currently_examined_path.choice_made];
-    let end_game_story_bit;
-    let index_of_compatible_condition = 0
-    for (const condition of condition_list){
-        end_game_story_bit = condition_type_conversion[condition.type](previously_examined_path_list, currently_examined_path, condition);
-        if (end_game_story_bit !== null){
-            break;
-        } 
-        index_of_compatible_condition += 1;
-    }
-    if (ignore_paragraph){
-        return {story_bit : end_game_story_bit};
-    } else {
-    const paragraph_type = condition_list[index_of_compatible_condition].paragraph;
-    return {story_bit : paragraph_determiner(end_game_story_bit, paragraph_type, paragraph_type_ledger), paragraph_type : paragraph_type};
-    }
-
-};
-
-function specific_event_checker (previously_examined_path_list, currently_examined_path, condition) {
-    for (const previous_path of previously_examined_path_list) {
-
-        if (previous_path.nth_event === condition.specification.nth_event || condition.specification.nth_event === undefined ) {
-            if (previous_path.name_of_event === condition.specification.event_name || condition.specification.event_name === undefined ) {
-                if (previous_path.choice_made === condition.specification.choice || condition.specification.choice === undefined ) {
-                    return condition.story_bit;
-                }
-            }    
-        }
-    }
-    return null;
-};
-
-function nth_event_checker (previously_examined_path_list, currently_examined_path, condition){
-    if (condition.specification.nth_this_event === currently_examined_path.nth_event || condition.specification.nth_this_event === undefined) {
-        if (condition.specification.event_before === undefined) {
-			return condition.story_bit;
-		} else {
-			for (const previous_path of previously_examined_path_list) {
-				if (previous_path.name_of_event === condition.specification.event_before) {
-					return condition.story_bit;
-				}
-			}
-			return null;
-		}
-    }
-
-    else {
-        return null;
-    }
-};
-
-function default_ending (previously_examined_path_list, currently_examined_path, condition){
-    return condition.story_bit;
-};
-
-const condition_type_conversion = {
-    "nth_event_checker" : nth_event_checker,
-    "specific_event_checker" : specific_event_checker,
-    "default_ending" : default_ending
+    return examined_event.end_story_bit_generator(examined_path_list, path, examined_event, [], true).story_bit;
 };
--- docs/Javascript/imported_modules/event.js
+++ docs/Javascript/imported_modules/event.js
@@ -20,7 +20,7 @@ export class Event {
     visible_choice_generator(path_taken){
         let choice_shown = [];
         for (const possible_answer of this.Possible_Answer_List) {
-            if (possible_answer.conditions === undefined || this.condition_type_conversion[possible_answer.conditions.type](path_taken, possible_answer.conditions)){
+            if (possible_answer.conditions === undefined || this.condition_type_conversion_choice[possible_answer.conditions.type](path_taken, possible_answer.conditions)){
                 choice_shown.push(possible_answer);
             }
         }
@@ -40,7 +40,92 @@ export class Event {
         return false;
     };
 
-    condition_type_conversion = {
+    condition_type_conversion_choice = {
         "specific_event_checker" : this.specific_event_checker_choice
+    };
+
+    end_story_bit_generator (previously_examined_path_list, currently_examined_path, paragraph_type_ledger, ignore_paragraph=false){
+        const condition_list = this.Ending[currently_examined_path.choice_made];
+        let end_game_story_bit;
+        let index_of_compatible_condition = 0;
+        for (const condition of condition_list){
+            end_game_story_bit = this.condition_type_conversion_ending[condition.type](previously_examined_path_list, currently_examined_path, condition);
+            if (end_game_story_bit !== null){
+                break;
+            } 
+            index_of_compatible_condition += 1;
+        }
+        if (ignore_paragraph){
+            return {story_bit : end_game_story_bit};
+        } else {
+            const paragraph_type = condition_list[index_of_compatible_condition].paragraph;
+            return {story_bit : paragraph_determiner(end_game_story_bit, paragraph_type, paragraph_type_ledger), paragraph_type : paragraph_type};
+        }
+    
+    };
+
+    specific_event_checker (previously_examined_path_list, currently_examined_path, condition) {
+        for (const previous_path of previously_examined_path_list) {
+    
+            if (previous_path.nth_event === condition.specification.nth_event || condition.specification.nth_event === undefined ) {
+                if (previous_path.name_of_event === condition.specification.event_name || condition.specification.event_name === undefined ) {
+                    if (previous_path.choice_made === condition.specification.choice || condition.specification.choice === undefined ) {
+                        return condition.story_bit;
+                    }
+                }    
+            }
+        }
+        return null;
+    };
+    
+    nth_event_checker (previously_examined_path_list, currently_examined_path, condition){
+        if (condition.specification.nth_this_event === currently_examined_path.nth_event || condition.specification.nth_this_event === undefined) {
+            if (condition.specification.event_before === undefined) {
+                return condition.story_bit;
+            } else {
+                for (const previous_path of previously_examined_path_list) {
+                    if (previous_path.name_of_event === condition.specification.event_before) {
+                        return condition.story_bit;
+                    }
+                }
+                return null;
+            }
+        }
+    
+        else {
+            return null;
+        }
+    };
+
+    default_ending (previously_examined_path_list, currently_examined_path, condition){
+        return condition.story_bit;
+    };
+
+    condition_type_conversion_ending = {
+        "nth_event_checker" : this.nth_event_checker,
+        "specific_event_checker" : this.specific_event_checker,
+        "default_ending" : this.default_ending
+    };
+
+    paragraph_determiner (end_game_story_bit, paragraph_type, paragraph_type_ledger){
+        let result;
+        let break_before = true;
+        if (paragraph_type_ledger[paragraph_type_ledger.length-1] === "last sentence"){
+            no_break_before = false;
+        }
+    
+        if (paragraph_type === "none" || paragraph_type === "new paragraph"){
+            result = end_game_story_bit;
+        } else if (paragraph_type === "last sentence" || paragraph_type === "standalone paragraph"){
+            result = end_game_story_bit+"<br><br>";
+        }
+    
+        if (paragraph_type === "standalone paragraph" || paragraph_type === "new paragraph"){
+            if (break_before){
+                result = "<br><br>"+result;
+            }
+        }
+    
+        return result;
     };
 };
