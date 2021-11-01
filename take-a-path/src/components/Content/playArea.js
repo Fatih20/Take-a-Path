487ef9637d7e27681f96bc39733643e079280bb3
Fatih20 <fnri39@protonmail.com>
2021-11-01 18:36:14 +0700

    Theming of play area

--- take-a-path/src/components/Content/playArea.js
+++ take-a-path/src/components/Content/playArea.js
@@ -4,7 +4,49 @@ import { useTheme } from '../../ThemeContext';
 import { useGameState, progressGameState  } from '../../GameStateContext';
 
 const Main = styled.div`
-    
+    align-items: center;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: ${({ gameState }) => {
+        if (gameState == "start"){
+            return "row";
+        } else if (gameState == "in-game"){
+            return "column";
+        } else if (gameState == "finished"){
+            return "none";
+        }
+    }};
+    min-height: ${({ gameState })=> {
+        if (gameState == "start" || gameState == "in-game"){
+            return "200px"
+        } else if (gameState == "finished"){
+            return "none"
+        }
+    }};
+    letter-spacing: ${ ({ gameState })=> {
+        if (gameState == "finished"){
+            return "1px";
+        } else {
+            return "none";
+        }
+    }};
+    line-height: ${ ({ gameState })=> {
+        if (gameState == "finished"){
+            return "1.5";
+        } else {
+            return "none";
+        }
+    }};
+    padding: ${({ gameState })=> {
+        if (gameState == "start" || gameState == "in-game"){
+            return "20px 40px"
+        } else if (gameState == "finished"){
+            return "25px"
+        }
+    }};
+    position: relative;
+    text-align: center;
+    width: 100%;
 `;
 
 function PlayArea(){
@@ -12,7 +54,7 @@ function PlayArea(){
     const gameState = useGameState();
 
     return(
-        <Main>
+        <Main darkTheme={darkTheme} gameState={gameState}>
 
         </Main>
     )
